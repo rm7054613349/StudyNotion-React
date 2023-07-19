aa830484c19d54b81c9642200cb33dd263483371
rm7054613349 <wwwritesh72660@gmail.com>
2023-07-19 18:36:59 +1100

    #

---
+++ src/components/Navbar.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+// .. means ap ak folder peche chle gye hai
+import logo from "../assets/Logo.svg"
+import {Link} from "react-router-dom"
+import {toast} from "react-hot-toast"
+
+
+const Navbar = (props) => {
+
+    let isLoggedIn = props.isLoggedIn;
+    let setIsLoggedIn = props.setIsLoggedIn;
+
+  return (
+
+    <div className='flex justify-between items-center w-11/12 max-w-[1160px] py-4 mx-auto'>
+
+
+        <Link to="/"> 
+            <img src={logo} alt="Logo" width={160} height={32} loading="lazy"/>
+        </Link>
+
+        <nav>
+
+            <ul className='text-richblack-100 flex gap-x-6'>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/">About</Link>
+                </li>
+                <li>
+                    <Link to="/">Contact</Link>
+                </li>
+            </ul>
+
+        </nav>
+
+
+
+        {/* Login - SignUp - LogOut - Dashboard */}
+
+        <div className='flex items-center gap-x-4'>
+
+             {/* jab user login nhi hoga tab hme ye and sinup button sho hoga */}
+            { !isLoggedIn &&
+                <Link to="/login">
+                    <button className='bg-richblack-800 text-richblack-100 py-[8px] 
+                    px-[12px] rounded-[8px] border border-richblack-700'>
+                        Log in
+                    </button>
+                </Link>
+            }
+
+
+             {/* sinup button per click krne se ham sinup button per phuch jayenge */}
+            { !isLoggedIn &&
+                <Link to="/signup">
+                    <button  className='bg-richblack-800 text-richblack-100 py-[8px] 
+                    px-[12px] rounded-[8px] border border-richblack-700'>
+                        Sign up
+                    </button>
+                </Link>
+            }
+
+
+            {/* jab user login hoga tab hme logout and dashboard button show hoga */}
+            { isLoggedIn &&
+                <Link to="/">
+                {/* jab ham is button per click krenge logout per to vah is login ki value ko false set krr dega and ak toast show krega */}
+                    <button onClick={() => {
+                        setIsLoggedIn(false);
+                        toast.success("Logged Out");
+                    }}
+                    className='bg-richblack-800 text-richblack-100 py-[8px] 
+                    px-[12px] rounded-[8px] border border-richblack-700'>
+                        Log Out
+                    </button>
+                </Link>
+            }
+            
+            { isLoggedIn &&
+                <Link to="/dashboard">
+                    <button
+                     className='bg-richblack-800 text-richblack-100 py-[8px] 
+                    px-[12px] rounded-[8px] border border-richblack-700'>
+                        Dashboard
+                    </button>
+                </Link>
+            }
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
